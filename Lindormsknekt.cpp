d40abf9223a12b0b91709eccf39bd5fe398ed24a
petterdj <petterdjupfeldt@gmail.com>
2018-10-19 23:20:29 +0200

    Öppna ett fönster

--- Lindormsknekt.cpp
+++ Lindormsknekt.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
-#include "lib/SFML-2.5.1/include/SFML/Graphics.hpp"
+#include "lib/SFML-2.5.1/include/SFML/Window.hpp"
 using namespace std;
 
 int main()
 {
     cout << "Hello, World!";
 
-    // sf::RenderWindow window(sf::VideoMode(200, 200), "IT VERKS!");
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+    
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 
     return 0;
 }
