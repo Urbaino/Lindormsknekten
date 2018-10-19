ceb405cd64bcbc83374ccc3e00719aec833bae09
Martin Larsson <martin.larsson.6@gmail.com>
2018-10-19 21:55:50 +0200

    Det bygger med länkar till SFML

--- Lindormsknekt.cpp
+++ Lindormsknekt.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "lib/SFML-2.5.1/include/SFML/Graphics.hpp"
 using namespace std;
 
 int main()
 {
     cout << "Hello, World!";
+
+    // sf::RenderWindow window(sf::VideoMode(200, 200), "IT VERKS!");
+
     return 0;
 }
